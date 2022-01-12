9f7c29e3dd94e524644dda95c9762a89d23a06e7
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-12 13:36:16 +0100

    Operaciones con variables

---
+++ src/CuatroVariablesAPP.java
@@ -0,0 +1,45 @@
+
+public class CuatroVariablesAPP {
+
+	public static void main(String[] args) {
+		
+		int varX=2;
+		int varY=5;
+		double varN=10.5;
+		double varM=15.5;
+		
+		System.out.println("Variable X = "+varX);
+		System.out.println("Variable Y = "+varY);
+		System.out.println("Variable N = "+varN);
+		System.out.println("Variable M = "+varM);
+		System.out.print("\n"); //Salto de linea
+		System.out.println("La suma de X + Y es = "+(varX + varY));
+		System.out.println("La diferencia de X - Y es = "+(varX - varY));
+		System.out.println("El producto de X * Y es = "+(varX * varY));
+		System.out.println("El cociente de X / Y es = "+(varX / varY));
+		System.out.println("El resto de X % Y es = "+(varX % varY));
+		System.out.print("\n");		
+		System.out.println("La suma de N + M es = "+(varN + varM));
+		System.out.println("La diferencia de N - M es = "+(varN - varM));
+		System.out.println("El producto de N * M es = "+(varN * varM));
+		System.out.println("El cociente de N / M es = "+(varN / varM));
+		System.out.println("El resto de N % M es = "+(varN % varM));
+		System.out.print("\n");
+		System.out.println("La suma de X + N es = "+(varX + varN));
+		System.out.print("\n");
+		System.out.println("El cociente de Y / M es = "+(varY / varM));
+		System.out.print("\n");
+		System.out.println("El resto de Y % M es = "+(varY % varM));
+		System.out.print("\n");		
+		System.out.println("El doble de la variable X es = "+(varX*2));
+		System.out.println("El doble de la variable Y es = "+(varY*2));
+		System.out.println("El doble de la variable N es = "+(varN*2));
+		System.out.println("El doble de la variable M es = "+(varM*2));
+		System.out.print("\n");	
+		System.out.println("La suma de todas las variables es = "+(varX+varY+varN+varM));
+		System.out.print("\n");		
+		System.out.println("El producto de todas las variables es = "+(varX*varY*varN*varM));
+
+	}
+
+}
